12da91a53e6db9ea63d89dc72c4c60951267b019
Pgvignesh <vignesh200399@gmail.com>
2026-02-19 10:13:38 +0530

    Add project.nuget.cache file with package dependencies and metadata

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/stocks")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBcontext _context;
+
+        public StockController(ApplicationDBcontext context)
+        {
+           _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.ToList();
+            return Ok(stocks);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
+    }
+}
